f16b38657f62e97132314136018aad72d22e0efb
Yordanos111 <jor.esh55@gmail.com>
2022-03-10 09:09:23 -0800

    My commit for task 6. Again.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long int li;
 	long long int lli;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(c));
-	printf("Size of an int: %zu byte(S)\n", sizeof(i));
-	printf("Size of a long int: %zu byte(S)\n", sizeof(li));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(llt));
-	printf("Size of a float: %zu bytes(s)\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(S)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(S)\n", sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(llt));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(f));
 	return (0);
 }
