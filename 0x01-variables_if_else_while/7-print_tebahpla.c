23f4b7934c58a63644c4292df1dc4fda9c8d4b18
Yordanos111 <jor.esh55@gmail.com>
2022-03-11 02:58:00 -0800

    My commit for task 7..

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - in prints the lowercase alphabet reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
