e36bf544f4cebbe55c37449c2713c6120f72a9b2
Yordanos111 <jor.esh55@gmail.com>
2022-03-11 01:38:15 -0800

    My commit for task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
