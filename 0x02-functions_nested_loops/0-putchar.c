e1cb8f4c7e2353807097f93a5b7881450fb2e8dc
Yordanos111 <jor.esh55@gmail.com>
2022-03-15 23:42:48 +0300

    my another commit, main.h and task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,15 @@
  * main -  program that prints
  *
  * Return: Always 0 (Success)
- */
-
+ **/
 int main(void)
 {
 	char yordanos[] = "_putchar";
-
 	int c;
 
 	for (c = 0; c < 8; c++)
 	{
-		_putchar(Yordanos[c]);
+		_putchar(yordanos[c]);
 	}
 	_putchar('\n');
 	return (0);
