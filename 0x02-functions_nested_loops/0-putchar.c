87380117ae6078276d05560c631e3e15d63ec25a
Yordanos111 <jor.esh55@gmail.com>
2022-03-15 14:58:59 +0300

    My commit for task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main -  program that prints
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *main = "_putchar";
+	{
+		while (*main)
+		{
+			_putchar(*putchat)
+		}
+	}
+}
+	return (0);
+}
