5dea18de6e1e3ae3179c079243e012190b53df98
Yordanos111 <jor.esh55@gmail.com>
2022-03-16 05:05:56 +0300

    My commit for task 12, after out put error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 
 	if (n > 15 || n < 0)
 		return;
-	for (i = 0; j <= n; j++)
+	for (i = 0; i <= n; j++)
 	{
 		k = i * j;
 		if (j == 0)
@@ -25,7 +25,7 @@ void print_times_table(int n)
 			_putchar(' ');
 			_putchar(k + '0');
 		}
-		else if (k >= 1 && k < 100)
+		else if (k >= 10 && k < 100)
 		{
 			_putchar(',');
 			_putchar(' ');
