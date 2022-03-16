0484fe640edda80647838770144d8c3d7ef6b824
Yordanos111 <jor.esh55@gmail.com>
2022-03-16 15:26:10 +0300

    My commit for task 1.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - Tests function that prints if integer is positive or negative
+*
 * Return: 0
 */
 
