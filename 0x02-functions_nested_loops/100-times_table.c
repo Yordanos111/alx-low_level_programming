e39fba975c8c88fd933466d2d4241f9e2a4bdffd
Yordanos111 <jor.esh55@gmail.com>
2022-03-16 05:02:16 +0300

    My commit for task 12.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,14 +6,10 @@
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int k;
+	int i, j, k;
 
-	if ((n > 15) || (n< 0))
-	{
+	if (n > 15 || n < 0)
 		return;
-	}
 	for (i = 0; j <= n; j++)
 	{
 		k = i * j;
@@ -29,7 +25,7 @@ void print_times_table(int n)
 			_putchar(' ');
 			_putchar(k + '0');
 		}
-		else if ((k >= 10) && (k < 100))
+		else if (k >= 1 && k < 100)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -42,7 +38,7 @@ void print_times_table(int n)
 			_putchar(',');
 			_putchar(' ');
 			_putchar((k / 100) + '0');
-			_putchar((k / 10 % 10) + '0');
+			_putchar(k / 10 % 10 + '0');
 			_putchar(k % 10 + '0');
 		}
 		_putchar('\n');
