87a4f8c7d9dce49eac0212323650338166279026
Yordanos111 <jor.esh55@gmail.com>
2022-03-16 04:11:55 +0300

    My commit for task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @i: takes an intiger
+ * @j: takes an intiger
+ * Return: result of i + j
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
