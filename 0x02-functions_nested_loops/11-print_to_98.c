8deaab732d595dc1d61ff19a0b1cff0368d4cc3f
Yordanos111 <jor.esh55@gmail.com>
2022-03-16 04:35:16 +0300

    My commit for task 11.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 
-/**print_to_98 - prints all natural numbers from n to 98
+/** print_to_98 - prints all natural numbers from n to 98
  * @n: takes in an intiger
+ *
  */
 void print_to_98(int n)
 {
 	int i;
 
-	if(n > 98)
+if (n > 98)
 	{
 		for (i = n; i > 97; i--)
 		{
