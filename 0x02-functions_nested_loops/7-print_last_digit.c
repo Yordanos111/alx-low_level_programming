06c34c39eb058b66dad5e01f39f244a94b713c64
Yordanos111 <jor.esh55@gmail.com>
2022-03-16 03:31:40 +0300

    My commit for task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - .rints the last digit of a number
+ * @c takes in a number
+ * Return: ourput
+ */
+int print_last_digit(int c)
+{
+	int ld = c % 10;
+
+	if (c < 0)
+	{
+		ld = ld * -1;
+	}
+	_putchar(ld + '0');
+	return (ld);
+}
