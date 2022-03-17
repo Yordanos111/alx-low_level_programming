710e652b765c5414e77144ee2c6843ce8efc8039
Yordanos111 <jor.esh55@gmail.com>
2022-03-17 14:36:15 +0300

    my commit for task 2. again

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,8 @@
 
 /**
  * mul - multiplies two integers
- * a: takes in an intiger
- * b: takes in an intiger
+ * @a: takes in an intiger
+ * @b: takes in an intiger
  * Return: multiple of a and b
  */
 int mul(int a, int b)
