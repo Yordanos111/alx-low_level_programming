00a19d877a04dff1084cc5fb840f28a98adc218b
Yordanos111 <jor.esh55@gmail.com>
2022-03-17 21:20:46 +0300

    My commit for Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,23 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i; 
 	int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j <= 14 ; j++)
 		{
-			_putchar('0' + j / 10);
-			_putchar('0' + j % 10);
+			if ((j >=0) && (j <= 9))
+			{
+				_putchar('0' + j);
+			}
+			else if ((j >= 10) && (j <= 14))
+			{
+				_putchar('0' + j / 10);
+				_putchar('0' + j % 10);
+			}
 		}
-		_putchar('\n');
+		_putchar ('\n');
 	}
 }
