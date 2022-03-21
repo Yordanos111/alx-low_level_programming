f978bab6053a5d53c28f8c10e3a490eda163fd59
Yordanos111 <jor.esh55@gmail.com>
2022-03-21 17:53:59 +0300

    My commit for task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to be converted to intigers
+ * Return: iniger
+ */
+int _atoi(char *s)
+{
+	int i;
+
+	i = atoi(s);
+	return(i);
+}
