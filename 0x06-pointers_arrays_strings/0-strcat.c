856271fb2741c7947caf08c119ed1ffc43104f9c
Yordanos111 <jor.esh55@gmail.com>
2022-03-24 22:52:09 +0300

    My commit for task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string
+ * @src: string
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
