f03a241f7b05ee2f9a1b0c8c61ca2005ebca980e
Yordanos111 <jor.esh55@gmail.com>
2022-03-24 23:29:01 +0300

    My commit for task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int comp;
+
+	comp = strcmp(s1, s2);
+	return (comp);
+}
