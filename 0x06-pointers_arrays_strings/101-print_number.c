f5c37421d510446c09f74bc8d1f7debb735ae22c
Yordanos111 <jor.esh55@gmail.com>
2022-03-25 23:43:13 +0300

    Task 10, again

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - prints intergers
+ * @n: takes interger
+ * Return: no argumnt
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar('0' + n % 10);
+}
