4ee814fd6b54c227f00af4a41e09177d68450f8d
Yordanos111 <jor.esh55@gmaial.com>
2022-04-08 19:52:08 +0300

    My commit for main.h; additional prototypes

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,5 +8,8 @@ int *array_range(int min, int max);
 int _putchar(char c);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void _is_zero(char *argv[]);
+char *_initialize_array(char *ar, int lar);
+char *_initialize_array(char *ar, int lar);
+int main(int argc, char *argv[]);
 
 #endif
