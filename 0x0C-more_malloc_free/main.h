b5a27aba5bd06f6a9ca41468a80b7ddef2f4e5d4
Yordanos111 <jor.esh55@gmaial.com>
2022-04-08 20:01:17 +0300

    main.h, again

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,7 +9,7 @@ int _putchar(char c);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void _is_zero(char *argv[]);
 char *_initialize_array(char *ar, int lar);
-char *_initialize_array(char *ar, int lar);
+int _checknum(char *argv[], int n);
 int main(int argc, char *argv[]);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
