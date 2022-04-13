fc92e065edac6a47773d0a49ae6dec1c7ac61392
Yordanos111 <jor.esh55@gmaial.com>
2022-04-13 21:04:26 +0300

    My commit for task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - prints a name
+ * @name: pointer
+ * @f: argument that is pointer
+ * @char *: argument that is array
+ * Return: No argument
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
