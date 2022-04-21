c94ec11cc76661be205e67d0ee6654df7ce8b6e8
Yordanos111 <jor.esh55@gmail.com>
2022-04-22 00:07:10 +0300

    My commit for task 5, correction

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,10 @@
  * myStartupFun - a function that that prints the statement:
  * You're beat! and yet, you must allow,\n
  * I bore my house upon my back!\n before the main function is executed
+ * @attribute: attribute
  */
-void myStartupFun(void)
+void myStartupFun (void) __attribute__ ((constructor));
+void myStartupFun (void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
