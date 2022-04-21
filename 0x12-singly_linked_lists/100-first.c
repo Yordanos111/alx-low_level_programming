0df799acc11641b3cbd89abf4f43918d03ad8731
Yordanos111 <jor.esh55@gmail.com>
2022-04-21 23:10:32 +0300

    My commit for task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include "stdio.h"
+/**
+ * myStartupFun - a function that that prints the statement:
+ * You're beat! and yet, you must allow,\n
+ * I bore my house upon my back!\n before the main function is executed
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
