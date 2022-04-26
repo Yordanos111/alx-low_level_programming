2b9ab42459d0dc225f399bb5cdcbcfa994ad2358
Yordanos111 <jor.esh55@gmail.com>
2022-04-26 21:28:21 +0300

    My commit for task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * free_listint_safe - a function that frees a listint_t list
+ * @h: the first node
+ * Return: the size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (len);
+}
