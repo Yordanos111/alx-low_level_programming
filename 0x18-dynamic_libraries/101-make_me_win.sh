e130d2b925f5e80670f68a38606da3a73bffd81a
Yordanos111 <jor.esh55@gmail.com>
2022-06-16 21:06:32 +0300

    My commit for dynamic_libraries.

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qO /tmp/jackpot.so https://github.com/SWaseni/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jackpot.so
+wget -qO /tmp/jackpot.so https://github.com/Yordanos111/alx-low_level_programming/tree/master/0x18-dynamic_libraries/jackpot.so
 export LD_PRELOAD=/tmp/jackpot.soi
